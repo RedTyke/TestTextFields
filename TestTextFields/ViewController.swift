0484b390d491094f6fd57f2177721f95df142498
RedTyke <john.mcniffe@hotmail.com>
2018-08-19 16:51:42 +0100

    Updated

--- TestTextFields/ViewController.swift
+++ TestTextFields/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    @IBAction func goButtonPressed(_ sender: Any) {
+        
+        outputLabel.text = textField.text
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.delegate = self
+        
+        // hide keyboard
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        gestureRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(gestureRecognizer)
     }
 
 
+    @objc func hideKeyboard(_ gestureRecognizer: UIGestureRecognizer) {
+        textField.resignFirstResponder()
+        print("Touching!")
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
 }
 
