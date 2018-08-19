362e0aab5e948eac4cb5f797d35a8dd635c33bf2
RedTyke <john.mcniffe@hotmail.com>
2018-08-19 11:37:35 +0100

    Initial Commit

---
+++ TestTextFields/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTextFields
+//
+//  Created by John McNiffe on 19/08/2018.
+//  Copyright © 2018 John McNiffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
